43cd3fed36b087b8779ef70aa9ce5e77db6a4a45
ibrauwt <145372356+ibrahimtorresw19@users.noreply.github.com>
2025-07-09 18:35:22 -0400

    Update inventarioController.php

--- app/Http/Controllers/inventarioController.php
+++ app/Http/Controllers/inventarioController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use App\Models\Categorias;
+use App\Models\categorias;
 use App\Models\inventario_Fisico;
 use Illuminate\Support\Facades\Auth;
 use App\Models\productos;
-use App\Models\Almacen;
+use App\Models\almacen;
 use App\Models\Proveedor;
 use App\Models\movimiento_de_inventario;
 
@@ -20,10 +20,10 @@ class inventarioController extends Controller
 {
 
       $userId = Auth::id(); 
-           $Countcategorias = Categorias::where('user_id', $userId)->count();
-        $CountProductos = Productos::where('user_id', $userId)->count();
-         $CountAlmacen=Almacen::where('user_id', $userId)->count();
-        $Countproveedores = Proveedor::where('user_id', $userId)->count();
+           $Countcategorias = categorias::where('user_id', $userId)->count();
+        $CountProductos = productos::where('user_id', $userId)->count();
+         $CountAlmacen=almacen::where('user_id', $userId)->count();
+        $Countproveedores = proveedor::where('user_id', $userId)->count();
           $CountInventarioFisico = inventario_Fisico::where('user_id', $userId)->count();
          $CountMovimiento = movimiento_de_inventario::where('user_id', $userId)->count();
 
