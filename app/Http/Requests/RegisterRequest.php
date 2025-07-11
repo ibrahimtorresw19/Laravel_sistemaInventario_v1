1f3824124b3d9f061061c6122c49b7f447427d1e
ibrauwt <145372356+ibrahimtorresw19@users.noreply.github.com>
2025-07-10 20:28:54 -0400

    Update RegisterRequest.php

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -26,7 +26,7 @@ class RegisterRequest extends FormRequest
 
 
             'name'=>'required|string|max:255',
-            'email'=>'required|string|email|max:255|unique:users',
+            'email'=>'required|string|email|max:255',
             'username'=>'required|string|max:255|unique:users',
             'password'=>'required|string|confirmed',
 
